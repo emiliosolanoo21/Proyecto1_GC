b8e5990fb844e531b2db1c3d6053ba16d7502a6d
emiliosolanoo21 <85262580+emiliosolanoo21@users.noreply.github.com>
2023-08-24 21:03:57 -0600

    actualización 3

--- Rasterizer2023.py
+++ Rasterizer2023.py
@@ -20,24 +20,24 @@ rend.glBackgroundTexture("background\desk.bmp")
 rend.glClearBackground()
 
 
-rend.directionalLight = (0,0,-1)
+""" rend.directionalLight = (0,0,-1) """
 
 # Cargamos los modelos que rederizaremos
-model1 = Model("models\model.obj",
+model1 = Model("models\bracelet.obj",
               translate = (-1,0,-5),
               scale = (1.5,1.5,1.5))
-model1.LoadTexture("textures\model.bmp")
-model1.LoadNormalMap("textures\model_normal.bmp")
+model1.LoadTexture("textures\bracelet.bmp")
+model1.LoadNormalMap("textures\braceletNormal.bmp")
 model1.SetShaders(shaders.vertexShader, shaders.normalMapShader)
 
-model2 = Model("models\model.obj",
+""" model2 = Model("models\hand.obj",
               translate = (1,0,-5),
               scale = (1.5,1.5,1.5))
-model2.LoadTexture("textures\model.bmp")
+model2.LoadTexture("textures\silk.bmp")
 model2.SetShaders(shaders.vertexShader, shaders.gouradShader)
-
+ """
 rend.glAddModel(model1)
-rend.glAddModel(model2)
+""" rend.glAddModel(model2) """
 
 # Se renderiza la escena
 rend.glRender()
